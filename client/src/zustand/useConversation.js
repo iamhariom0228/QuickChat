2bfaccca8d183e4473d7a4117ca979bf0c106b92
Hariom Sharma <89256126+Hariom-Starlink90@users.noreply.github.com>
2024-06-20 18:54:22 +0530

    create useConversation hook for global access

---
+++ client/src/zustand/useConversation.js
@@ -0,0 +1,10 @@
+import { create } from 'zustand'
+
+const useConversation = create((set) => ({
+    selectedConversation: null,
+    setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
+    conversations: [],
+    setConversations: (conversations) => set({ conversations }),
+}))
+
+export default useConversation
