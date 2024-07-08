2b39d9d718bd3066b9f07fcefc9fae0a22295d3d
Hariom Sharma <89256126+Hariom-Starlink90@users.noreply.github.com>
2024-07-08 19:10:52 +0530

    fix message array not getting updated

--- client/src/hooks/useSendMessage.js
+++ client/src/hooks/useSendMessage.js
@@ -1,35 +1,43 @@
-import { useState } from "react"
+import { useState } from "react";
 import useConversation from "../zustand/useConversation";
 import { toast } from "react-hot-toast";
 
 const useSendMessage = () => {
-    const [loading, setLoading] = useState(false);
-    const { messages, setMessages, selectedConversation } = useConversation();
+  const [loading, setLoading] = useState(false);
+  const { messages, setMessages, selectedConversation } = useConversation();
+  console.log(messages)
+  const sendMessage = async (message) => {
+    setLoading(true);
 
-    const sendMessage = async (message) => {
-        setLoading(true);
-
-        try {
-            const res = await fetch(`/api/messages/send/${selectedConversation._id}`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ message }),
-            });
-
-            const data = await res.json();
-            if (!res.ok) throw new Error(data.message);
-            
-            setMessages([...messages, data.newMessage]);
-        } catch (error) {
-            toast.error(error.message);
-        } finally {
-            setLoading(false);
+    try {
+      const res = await fetch(
+        `/api/messages/send/${selectedConversation._id}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ message }),
         }
-    }
+      );
 
-    return { sendMessage, loading };
-}
+      const data = await res.json();
+      console.log(data);
+      if (!res.ok) throw new Error(data.error);
+      console.log(data);
+      
+      const newmessages = messages;
+      newmessages.push(data);
+      
+      setMessages(newmessages);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { sendMessage, loading };
+};
 
 export default useSendMessage;
